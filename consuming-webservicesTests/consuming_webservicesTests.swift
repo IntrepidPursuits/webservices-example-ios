f939bcb633ba1f3ba1a400b005982e0c7e3f93b1
Stephen Wingchi Wong <stephwwong@gmail.com>
2017-06-07 01:45:16 -0400

    Adds view model init test

--- consuming-webservicesTests/consuming_webservicesTests.swift
+++ consuming-webservicesTests/consuming_webservicesTests.swift
@@ -11,26 +11,17 @@ import XCTest
 
 class consuming_webservicesTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testInit() {
+        let colorChangeExpectation = expectation(description: "Expect the color change callback to get fired")
+        func colorDidSetTest(color: UIColor) {
+            colorChangeExpectation.fulfill()
+        }
+        let viewModel = ViewModel(colorDidSet: colorDidSetTest)
+        viewModel.color = UIColor.white
+        waitForExpectations(timeout: 5) { error in
+            if let _ = error {
+                XCTFail()
+            }
         }
     }
-    
 }
