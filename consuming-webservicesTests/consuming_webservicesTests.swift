dec2f946d35460b49fd35aae4c0ad67c7960fa91
Alex Persian <alex_persian@me.com>
2018-01-07 17:21:14 -0500

    Update existing tests and add more converage for changed functions

--- consuming-webservicesTests/consuming_webservicesTests.swift
+++ consuming-webservicesTests/consuming_webservicesTests.swift
@@ -19,9 +19,63 @@ class consuming_webservicesTests: XCTestCase {
         let viewModel = ViewModel(colorDidSet: colorDidSetTest)
         viewModel.color = UIColor.white
         waitForExpectations(timeout: 5) { error in
-            if let _ = error {
+            if error != nil {
                 XCTFail()
             }
         }
+    }
+
+    func testGetColor() {
+        let colorChangeExpectation = expectation(description: "Expect the color change callback to get fired")
+        func colorDidSetTest(color: UIColor) {
+            colorChangeExpectation.fulfill()
+        }
+        let viewModel = ViewModel(colorDidSet: colorDidSetTest)
+        viewModel.getColor()
+        waitForExpectations(timeout: 5) { error in
+            if error != nil {
+                XCTFail()
+            }
+        }
+    }
+
+    func testSetColor() {
+        let colorChangeExpectation = expectation(description: "Expect the color change callback to get fired")
+        func colorDidSetTest(color: UIColor) {
+            colorChangeExpectation.fulfill()
+        }
+        let viewModel = ViewModel(colorDidSet: colorDidSetTest)
+        viewModel.setColor(UIColor.white)
+        waitForExpectations(timeout: 5) { error in
+            if error != nil {
+                XCTFail()
+            }
+        }
+    }
+
+    func testRGBAInit() {
+        do {
+            let color = try UIColor(rgba_throws: "#C0FFEEFF")
+            XCTAssertEqual(color.hexString(), "#C0FFEEFF")
+        }
+        catch {
+            XCTFail()
+        }
+    }
+
+    func testARGB2RGBA() {
+        let unconverted: String = "#FFC0FFEE"
+        guard let converted: String = unconverted.argb2rgba() else {
+            XCTFail()
+            return
+        }
+        XCTAssertEqual(converted, "#C0FFEEFF")
+
+        let shortUnconverted: String = "#FC0F"
+        guard let shortConverted: String = shortUnconverted.argb2rgba() else {
+            XCTFail()
+            return
+        }
+        XCTAssertEqual(shortConverted, "#C0FF")
     }
 }
