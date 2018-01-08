d91626452bdbafb153fba9631633e7bb32d0a24f
Alex Persian <alex_persian@me.com>
2018-01-07 20:03:14 -0500

    Adds ColorData model

---
+++ consuming-webservices/Models/ColorData.swift
@@ -0,0 +1,14 @@
+//
+//  ColorData.swift
+//  consuming-webservices
+//
+//  Created by Persian, Alex on 1/7/18.
+//  Copyright © 2018 Intrepid Pursuits. All rights reserved.
+//
+
+import Foundation
+
+struct ColorData: Codable {
+    let color: String
+    let name: String
+}
